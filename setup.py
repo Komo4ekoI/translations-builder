402bde2f66a81dcb78a03cab67fc8dae5774a2f2
Komo4ek <laivhakin@gmail.com>
2024-09-06 11:17:46 +0200

    Fix: Update required python version

--- setup.py
+++ setup.py
@@ -29,8 +29,6 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -40,6 +38,6 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Software Development :: Code Generators",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=["PyYAML==6.0.2", "black==24.8.0"],
 )
